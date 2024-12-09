86451470b6ab1f25338092a7c47aa7024175499f
AnswerShy <117408562+AnswerShy@users.noreply.github.com>
2024-12-09 14:55:31 +0200

    Fix[Component, Visual](Card): Fixed Card skeleton size, added count of skeleton cards

--- src/components/Shared/Card/CardSkeleton.tsx
+++ src/components/Shared/Card/CardSkeleton.tsx
@@ -1,17 +1,15 @@
 import styles from "./Card.module.css";
 
-const CardSkeletonBlock = ({countOfCards = 1}: {countOfCards?: number}) => {
+const CardSkeletonBlock = ({ countOfCards = 1 }: { countOfCards?: number }) => {
     return (
-        <section style={{ justifyContent: "center", alignContent: "center", height: "auto" }}>
-            <section style={{ padding: "0", display: "grid", gridTemplateColumns: "repeat(6, var(--posterWidth))", gap: "15px", height: "auto" }}>
-                {Array.from({ length: countOfCards }).map((_, i) => (
-                    <div key={i} className={`${styles.card}`}>
-                        <div className={`${styles.cardContainer} ${styles.load}`} />
-                        <p className={`${styles.cardTitle} ${styles.load}`}>title</p>
-                    </div>
-                ))}
-            </section>
-        </section>
+        <>
+            {Array.from({ length: countOfCards }).map((_, i) => (
+                <div key={i} className={`${styles.card}`}>
+                    <div className={`${styles.cardContainer} ${styles.load} min-h-80`} />
+                    <p className={`${styles.cardTitle} ${styles.load}`}>title</p>
+                </div>
+            ))}
+        </>
     );
 };
 
