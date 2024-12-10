679ad482bf79e4a4fc86b029083d9d73e0ad557e
AnswerShy <117408562+AnswerShy@users.noreply.github.com>
2024-12-10 23:26:12 +0200

    Global(Linter)

--- src/utils/custom/onClickAnimation.tsx
+++ src/utils/custom/onClickAnimation.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 interface TransitionLinkProps extends React.HTMLAttributes<HTMLDivElement> {
     url: string;
@@ -9,22 +9,28 @@ interface TransitionLinkProps extends React.HTMLAttributes<HTMLDivElement> {
     isVision?: () => void | null;
 }
 
-const TransitionLink: React.FC<TransitionLinkProps> = ({url, isVision = null, children, className, ...props}) => {
-    const router = useRouter()
+const TransitionLink: React.FC<TransitionLinkProps> = ({ url, isVision = null, children, className }) => {
+    const router = useRouter();
 
     const doLink = async (url: string, event: React.MouseEvent) => {
         event.preventDefault();
         if (isVision) {
             isVision();
         }
-        document.getElementById("transition")?.classList.remove("hidden")
-        await sleep(1000)
-        router.push(url)
-        await sleep(1000)
-        document.getElementById("transition")?.classList.add("hidden")
-    }
+        document.getElementById("transition")?.classList.remove("hidden");
+        await sleep(1000);
+        router.push(url);
+        await sleep(1000);
+        document.getElementById("transition")?.classList.add("hidden");
+    };
 
-    return <Link href={url} className={className}><div onClick={(event) => doLink(url, event)} {...props}>{children}</div></Link>
-}
+    return (
+        <Link href={url} className={className}>
+            <div onClick={(event) => doLink(url, event)} className="relative w-full h-full rounded-xl">
+                {children}
+            </div>
+        </Link>
+    );
+};
 
-export default TransitionLink
+export default TransitionLink;
--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -2,3 +2,4 @@ export { default as KeyboardArrowDownIcon } from "@mui/icons-material/KeyboardAr
 export { default as NotificationsOutlinedIcon } from "@mui/icons-material/NotificationsOutlined";
 export { default as ArrowForwardIcon } from "@mui/icons-material/ArrowForward";
 export { default as MenuIcon } from "@mui/icons-material/Menu";
+export { default as Person } from "@mui/icons-material/Person";
