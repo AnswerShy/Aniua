d6697791d44333a985ec7a9fde7b1d2363fa10c0
AnswerShy <qqeanswershy@gmail.com>
2025-04-16 13:21:01 +0300

    :sparkles:COSMETIC [Fonts, Dark/light theme]

--- src/components/UI/Typography/Typography.tsx
+++ src/components/UI/Typography/Typography.tsx
@@ -4,11 +4,11 @@ type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'body1' | 'button';
 
 export const variantMap: Record<Variant, { tag: keyof JSX.IntrinsicElements; className: string }> =
   {
-    h1: { tag: 'h1', className: 'text-6xl font-extrabold tracking-tight text-textCol' },
-    h2: { tag: 'h2', className: 'text-4xl font-normal tracking-tight first:mt-0 text-textCol' },
-    h3: { tag: 'h3', className: 'text-2xl font-normal tracking-tight' },
-    h4: { tag: 'h4', className: 'text-xl font-normal tracking-tight' },
-    body1: { tag: 'p', className: 'text-2xl leading-7 align-center' },
+    h1: { tag: 'h1', className: 'text-4xl font-extrabold tracking-tight text-textCol' },
+    h2: { tag: 'h2', className: 'text-2xl font-normal tracking-tight first:mt-0 text-textCol' },
+    h3: { tag: 'h3', className: 'text-lg font-normal tracking-tight' },
+    h4: { tag: 'h4', className: 'text-sm font-normal tracking-tight' },
+    body1: { tag: 'p', className: 'leading-7 align-center text-textSemiCol' },
     button: { tag: 'button', className: 'text-xl leading-6 font-light tracking-[0] text-textCol' },
   };
 
