e6bd49ccd9045d15a42342a60dbc213941601e1a
AnswerShy <qqeanswershy@gmail.com>
2025-04-16 13:30:59 +0300

    :hammer:FIX [LoaderList]
    Starts from first page after reload

--- src/app/List/Components/ListSectionLoader.tsx
+++ src/app/List/Components/ListSectionLoader.tsx
@@ -2,24 +2,23 @@
 
 import { Section, Card } from '@/components/UI/UIComponents';
 import { useInView } from 'react-intersection-observer';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import FetchServiceInstance from '@/app/api';
-
-let page = 1;
 
 const ListSectionLoader = () => {
   const { ref, inView } = useInView();
   const [anime, setAnime] = useState<AnimeDataInterface[]>([]);
   const [isEnd, setIsEnd] = useState(false);
+  const pageRef = useRef(1);
 
   useEffect(() => {
     if (inView && !isEnd) {
       const fetchMore = async () => {
         try {
-          const moreAnime = await FetchServiceInstance.fetchAnimeList(page);
+          const moreAnime = await FetchServiceInstance.fetchAnimeList(pageRef.current);
           setAnime((prev) => [...prev, ...moreAnime]);
           if (moreAnime.length < 18) setIsEnd(true);
-          page++;
+          pageRef.current++;
         } catch (err) {
           console.error(err);
         }
@@ -36,7 +35,7 @@ const ListSectionLoader = () => {
           <Card key={index} image={el.poster} title={el.title} slug={el.slug}></Card>
         ))}
       </Section>
-      {isEnd ? <h1>END OF LIST</h1> : <div className="w-full h-10" ref={ref}></div>}
+      {isEnd ? <h2>END OF LIST</h2> : <div className="w-full h-10" ref={ref}></div>}
     </>
   );
 };
