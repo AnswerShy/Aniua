7194ec54759368773fa0017c84b02fe8955a79ce
AnswerShy <qqeanswershy@gmail.com>
2025-04-18 22:45:22 +0300

    :sparkles:COSMETIC [Card]
    Card skeleton fix

--- src/components/UI/Card/CardSkeleton.tsx
+++ src/components/UI/Card/CardSkeleton.tsx
@@ -1,12 +1,14 @@
+import clsx from 'clsx';
+import { TypographyType } from '../UIComponents';
 import styles from './Card.module.css';
 
 const CardSkeletonBlock = ({ countOfCards = 1 }: { countOfCards?: number }) => {
   return (
     <>
       {Array.from({ length: countOfCards }).map((_, i) => (
-        <div key={i} className={`${styles.card}`}>
-          <div className={`${styles.cardContainer} ${styles.load} min-h-80`} />
-          <p className={`${styles.cardTitle} ${styles.load}`}>title</p>
+        <div className={styles.card} key={i}>
+          <div className={clsx(styles.cardImage, styles.load)} />
+          <p className={clsx(styles.cardTitle, TypographyType['button'].className)}>Load</p>
         </div>
       ))}
     </>
