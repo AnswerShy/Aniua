a5b7e90f0e4d43484283400e32b224a5b4e286dc
AnswerShy <qqeanswershy@gmail.com>
2025-06-23 12:11:41 +0300

    :zap:UPDATE [Docker]
    Fixed cache

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+USE_NGROK=false
+
+for arg in "$@"; do
+  if [ "$arg" == "--ngrok" ]; then
+    USE_NGROK=true
+  fi
+done
+
 docker compose down -v
 docker container prune -f
-docker compose -f compose.full.yaml build --no-cache frontend
-docker compose -f compose.full.yaml up -d frontend
+docker compose -f compose.prod.yaml build --no-cache frontend
+docker compose -f compose.prod.yaml up -d frontend
+
+if [ "$USE_NGROK" = true ]; then
+  echo "Starting ngrok tunnel..."
+  ngrok http --url=honest-octopus-driving.ngrok-free.app 3000
+fi
