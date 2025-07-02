0ffb4a7aba02f22303f1bc656e0e72ced3ea07be
AnswerShy <qqeanswershy@gmail.com>
2025-07-02 23:55:22 +0300

    :hammer:FIX [route/list]
    Deleted unnecesasry function

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -11,16 +11,7 @@ export async function GET(req: NextRequest) {
 
     const finalRes = await res.json();
 
-    // avoid api.aniua filter issue
-    const uniqueTitlesMap = new Map<number, AnimeDataInterface>();
-    for (const title of finalRes.titles) {
-      if (!uniqueTitlesMap.has(title.id)) {
-        uniqueTitlesMap.set(title.id, title);
-      }
-    }
-    const uniqueTitles = Array.from(uniqueTitlesMap.values());
-
-    return NextResponse.json({ ...finalRes, titles: uniqueTitles });
+    return NextResponse.json({ ...finalRes });
   } catch (e) {
     return NextResponse.json({ message: 'Internal server error', error: e }, { status: 500 });
   }
