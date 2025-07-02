8f469538cf25789d5b86069c805a1e5e10cf3d7b
AnswerShy <qqeanswershy@gmail.com>
2025-07-02 23:56:35 +0300

    :hammer:FIX [Checkbox, Pagination]
    moved to client
    
    checked prop required

--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import styles from './Checkbox.module.css';
 interface checkboxProps {
   title: string;
+  checked: boolean;
   onChange?: () => void;
-  checked?: boolean;
 }
 
-function Checkbox({ title, onChange, checked }: checkboxProps) {
+function Checkbox({ title, onChange, checked, ...props }: checkboxProps) {
   return (
     <label className={styles.checkbox}>
-      <input checked={checked} type="checkbox" onChange={onChange} className={styles.checkinput} />
+      <input
+        checked={checked}
+        type="checkbox"
+        onChange={onChange}
+        {...props}
+        className={styles.checkinput}
+      />
       {title}
     </label>
   );
