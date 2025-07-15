1549bb958a1d56086ae55294ea8b71e5f8a280d2
AnswerShy <qqeanswershy@gmail.com>
2025-07-15 15:40:19 +0300

    :sparkles:COSMETIC [Shine and Load Animation fixed]

--- src/components/Banner/FullScreenBannerSkeleton.tsx
+++ src/components/Banner/FullScreenBannerSkeleton.tsx
@@ -4,12 +4,16 @@ import styles from './FullScreenBanner.module.css';
 const BannerSkeleton = () => {
   return (
     <Section typeOfSection={'BannerSection'}>
-      <div className={styles.bannerInfoContainer}>
-        <h1 className={`${styles.bannerTitle} ${styles.load}`}>TITLE</h1>
+      <div className={styles.bannerInfoContainer} style={{ width: '50%' }}>
+        <h1 className={`${styles.load}`} style={{ width: '50%' }}>
+          TITLE
+        </h1>
         <div className={styles.bannerBaseInfo}>
           <p className={styles.load}>0000</p>
           <span>•</span>
-          <p className={styles.load}>genre, genre, genre</p>
+          <p className={styles.load} style={{ width: '100%' }}>
+            genre, genre, genre
+          </p>
         </div>
         <p className={`${styles.bannerDescriptionLoad} ${styles.load}`}>...</p>
       </div>
