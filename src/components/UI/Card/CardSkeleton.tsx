b3d942e5b20dda5acd4f9bee403039efebc87880
AnswerShy <qqeanswershy@gmail.com>
2025-07-19 23:29:52 +0300

    :sparkles:COSMETIC [Card]
    Updated card view. Again

--- src/components/UI/Card/CardSkeleton.tsx
+++ src/components/UI/Card/CardSkeleton.tsx
@@ -6,7 +6,7 @@ const CardSkeletonBlock = ({ countOfCards = 1 }: { countOfCards?: number }) => {
   return (
     <>
       {Array.from({ length: countOfCards }).map((_, i) => (
-        <div className={clsx(styles.cardcontainer, 'w-[400px]')} key={i}>
+        <div className={clsx(styles.cardcontainer)} key={i}>
           <div className={clsx('w-[400px] h-[750px]', styles.cardImage, styles.load)} />
           <p className={clsx(styles.cardText, TypographyType['button'].className)}>Load</p>
         </div>
